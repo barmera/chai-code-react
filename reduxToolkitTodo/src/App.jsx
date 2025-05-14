f5ecc929f9b62eb1dc321054220dda6e33b4dc00
barmera <barmera@gmail.com>
2025-05-14 17:23:28 +0530

    - redux project completed

--- reduxToolkitTodo/src/App.jsx
+++ reduxToolkitTodo/src/App.jsx
@@ -1,9 +1,13 @@
 import "./App.css";
+import AddTodo from "./components/addTodo";
+import Todos from "./components/Todos";
 
 function App() {
   return (
     <>
       <h1 className="text-3xl text-center">Redux ToolKit</h1>
+      <AddTodo />
+      <Todos />
     </>
   );
 }
